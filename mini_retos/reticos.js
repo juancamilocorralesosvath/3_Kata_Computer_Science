1c4ab26622d93d0180d3f195522d3f53313004da
Juan Camilo Corrales Osvath <juancamilocorra@gmail.com>
2020-09-14 22:05:54 -0500

    add longestCountry function

--- mini_retos/reticos.js
+++ mini_retos/reticos.js
@@ -1,3 +1,5 @@
+// palabra palindroma
+
 const palindrome = (word) => {
     console.log(word.length);
     let counter = 1; 
@@ -18,3 +20,24 @@ const palindrome = (word) => {
 
 palindrome("ana");
 palindrome("reconocer");
+
+// paises
+
+const paises = ["Mexico", "panama", "El salvador", "Costa Rica", "Colombia", "estoy poniendo algo larguisimo para probar jje"];
+
+const longestCountry = (array) => { 
+    let max = Number.MIN_VALUE;
+    let country = "";
+
+    for(let m = 0; m < array.length; m++){ 
+    
+        if(array[m].length > max){ 
+          max = array[m].length;
+          country = array[m];
+        }
+    }
+
+    return country;
+}
+
+console.log(longestCountry(paises));
